aa2ae9bc5316f7161f823ca8507779a68bf9fe0c
bombermoto <51003053+bombermoto@users.noreply.github.com>
2025-05-08 22:39:26 +0300

    Delete static directory

--- static/script.js
+++
@@ -1,9 +0,0 @@
-function opencloseForm() {
-	var poll = document.getElementById("poll");
-	if (poll.style.display == "none" || poll.style.display == "") {
-		poll.style.display = "block";
-	} else if (poll.style.display == "block") {
-		poll.style.display = "none";
-	}	
-}
-
